e74789fca730fcdc462884c80bb7cf8df19c69f1
Kenji Akiyama <artifactsauce@gmail.com>
2014-12-25 01:38:52 +0900

    define empty methods as a CLI with Thor.

--- lib/operating_report.rb
+++ lib/operating_report.rb
@@ -1,5 +1,25 @@
 require "operating_report/version"
+require "thor"
 
 module OperatingReport
-  # Your code goes here...
+  class CLI < Thor
+    def initialize(*args)
+      super
+    end
+
+    desc "init", "create a config file."
+    def init
+    end
+
+    desc "create [PERIOD]", "create a report. (parameter required)"
+    def create(period)
+    end
+
+    private
+    def _load_config
+    end
+
+    def _fetch_via_api(path)
+    end
+  end
 end
