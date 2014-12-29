07c5c0afd0690eb4255d3a3ad2fc59b9c1bda810
Kenji Akiyama <artifactsauce@gmail.com>
2014-12-29 15:49:10 +0900

    enable to output the weekly report.

--- lib/operating_report/cli.rb
+++ lib/operating_report/cli.rb
@@ -78,6 +78,11 @@ module OperatingReport
         total_time += x['duration'].to_i
       end
 
+      if period == 'weekly' then
+        title = _generate_title(start_date, end_date)
+        puts "Title: #{title}"
+      end
+
       body.each do |x, y|
         case period
         when 'daily' then
@@ -108,8 +113,20 @@ module OperatingReport
     def _find_friday(t)
       loop do
         return Time.new(t.year, t.mon, t.day, 23, 59, 59) if t.friday?
-        t = t + (60 * 60 * 24)
+        if t.saturday? || t.sunday? then
+          t = t - (60 * 60 * 24)
+        else
+          t = t + (60 * 60 * 24)
+        end
       end
+    end
+
+    def _generate_title(start_date, end_date)
+      _get_formated_date(start_date) + ' - ' + _get_formated_date(end_date)
+    end
+
+    def _get_formated_date(t)
+      t.strftime('%Y/%m/%d')
     end
   end
 end
